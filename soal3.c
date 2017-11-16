6dd5b5da7316cf128c1a96fed4102fa651982d94
ryanuarmd <32842859+ryanuarmd@users.noreply.github.com>
2017-11-16 19:47:32 +0700

    Yo semangat yo!
    Fungsinya nambah

--- soal3.c
+++ soal3.c
@@ -1,3 +1,4 @@
+#define FUSE_USE_VERSION 28
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
@@ -5,4 +6,29 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
-#include <sys/statfs.h>
+#include <sys/time.h>
+
+static const char *dirpath ="home/yanuar/Downloads"
+
+static int xmp_getdir(const char *path, fuse_dirh_t h, fuse_dirfil_t filler)
+{
+    DIR *dp;
+    struct dirent *de;
+    int res = 0;
+    char fpath[1000];
+    sprintf(fpath, "%s%s", dirpath, path);
+    dp = opendir(fpath);
+    if(dp == NULL)
+        return -errno;
+
+    while((de = readdir(dp)) != NULL)
+    {
+        res = filler(h, de->d_name, de->d_type);
+        if(res != 0)
+            break;
+    }
+
+    closedir(dp);
+    return res;
+}
+
